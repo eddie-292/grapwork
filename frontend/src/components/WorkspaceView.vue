41bf53a4f7ff43a9ca698c6872df0af5244cf819
Eddie <eddie_292@163.com>
2026-02-22 23:43:12 +0800

    1

--- frontend/src/components/WorkspaceView.vue
+++ frontend/src/components/WorkspaceView.vue
@@ -424,7 +424,7 @@ defineExpose({
       <svg width="48" height="48" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="1.5">
         <path d="M22 19a2 2 0 0 1-2 2H4a2 2 0 0 1-2-2V5a2 2 0 0 1 2-2h5l2 3h9a2 2 0 0 1 2 2z"/>
       </svg>
-      <span>请先在聊天界面选择一个文件夹</span>
+      <span>选择一个文件夹作为工作空间</span>
     </div>
 
     <!-- 文件列表 -->
